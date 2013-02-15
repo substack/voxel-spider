dfd2165e352edf3ea5a4d8af2243018d756325df
James Halliday <mail@substack.net>
2013-02-15 20:46:35 +1000

    abdomen and head

--- index.js
+++ index.js
@@ -10,24 +10,36 @@ module.exports = function (game) {
 function Spider (game) {
     this.game = game;
     var T = game.THREE;
-    var mesh = this.mesh = new T.Mesh(
+    
+    var body = new T.Object3D;
+    
+    var abdomen = new T.Mesh(
         new T.CubeGeometry(10, 10, 10),
         new T.MeshLambertMaterial({
             color: 0x200830,
             ambient: 0x200830
         })
     );
-    mesh.position.set(0, 5, 0);
-    var container = new T.Object3D;
-    container.add(mesh);
+    abdomen.position.set(0, 5, 0);
+    body.add(abdomen);
+    
+    var head = new T.Mesh(
+        new T.CubeGeometry(5, 5, 5),
+        new T.MeshLambertMaterial({
+            color: 0x200830,
+            ambient: 0x200830
+        })
+    );
+    head.position.set(0, 2.5, 7.5);
+    body.add(head);
     
     var dims = new T.Vector3(10, 10, 10);
-    var item = this.item = game.makePhysical(container, dims);
+    var item = this.item = game.makePhysical(body, dims);
     
     item.yaw.position.y = 200;
     item.subjectTo(new T.Vector3(0, -0.00009, 0));
     
-    game.scene.add(container);
+    game.scene.add(body);
     game.addItem(item);
 }
 
