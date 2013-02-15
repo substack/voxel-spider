544e92422069810bffd5652dc6f25027692c9942
James Halliday <mail@substack.net>
2013-02-15 22:55:49 +1000

    legs go up and down

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function Spider (game) {
             ambient: 0x200830
         })
     );
-    abdomen.position.set(0, 5, 0);
+    abdomen.position.set(0, 10, 0);
     body.add(abdomen);
     
     var head = new T.Mesh(
@@ -30,8 +30,49 @@ function Spider (game) {
             ambient: 0x200830
         })
     );
-    head.position.set(0, 2.5, 7.5);
+    head.position.set(0, 8, 7.5);
     body.add(head);
+    
+    var legs = [];
+    for (var side = 0; side <= 1; side++) {
+        for (var i = 0; i < 4; i++) {
+            var leg = new T.Object3D;
+            leg.position.z = i * 1.5;
+            leg.state = i % 2;
+            leg.position.y = leg.state + 3;
+            
+            legs.push(leg);
+            body.add(leg);
+            
+            var horiz = new T.Mesh(
+                new T.CubeGeometry(5, 1, 1),
+                new T.MeshLambertMaterial({
+                    color: 0x200830,
+                    ambient: 0x200830
+                })
+            );
+            horiz.position.x = 7.5 - side * 15;
+            horiz.position.y = 4;
+            leg.add(horiz);
+            
+            var vert = new T.Mesh(
+                new T.CubeGeometry(1, 5, 1),
+                new T.MeshLambertMaterial({
+                    color: 0x200830,
+                    ambient: 0x200830
+                })
+            );
+            vert.position.x = 10 - side * 20;
+            vert.position.y = 1.5;
+            leg.add(vert);
+        }
+    }
+    setInterval(function () {
+        legs.forEach(function (leg) {
+            leg.state = !leg.state;
+            leg.position.y = leg.state + 3;
+        });
+    }, 100);
     
     var dims = new T.Vector3(10, 10, 10);
     var item = this.item = game.makePhysical(body, dims);
@@ -54,7 +95,3 @@ Spider.prototype.move = function (x, y, z) {
     this.item.velocity.y += y;
     this.item.velocity.z += z;
 };
-
-Spider.prototype.jump = function () {
-    this.item.velocity.y = 10;
-}
