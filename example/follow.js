384305d023d452b81f0eeb7621e7cd5c4f2cfbab
James Halliday <mail@substack.net>
2013-02-16 00:49:51 +1000

    spider jumps after you

--- example/follow.js
+++ example/follow.js
@@ -26,6 +26,13 @@ setInterval(function () {
     var b = spider.position;
     var angle = Math.atan2(a.x - b.x, a.z - b.z);
     spider.rotation.y = angle;
+    
+    var pt = spider.position.clone();
+    pt.x += game.cubeSize / 2 * Math.sin(angle);
+    pt.z += game.cubeSize / 2 * Math.cos(angle);
+    if (game.getBlock(pt)) {
+        spider.move(0, 1, 0.5);
+    }
 }, 1000);
 
 setInterval(function () {
